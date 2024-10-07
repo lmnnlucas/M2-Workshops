c768b984225d27f84e08f7c4f0acf1673212641b
Lucas Le Menn <lucas.le-menn@etu.u-bordeaux.fr>
2024-10-07 12:00:25 +0200

    03: Enregistrer modifications dans la mémoire

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 
-const express = require('express')
-const app = express()
-const path = require("path")
-const ejs = require('ejs')
-var bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const path = require("path");
+const ejs = require('ejs');
+var bodyParser = require('body-parser');
 const repository = require('./inMemoryWorkshop');
 //const repository = require("./mongoWorkshop");
 
 repository.init().then(() => {
 
-    app.use(bodyParser.urlencoded({ extended: false }))
+    app.use(bodyParser.urlencoded({ extended: false }));
 
     // set the view engine to ejs
     app.set('view engine', 'ejs');
@@ -22,49 +22,67 @@ repository.init().then(() => {
         .then(workshops => {
             res.render("index", {
                 workshops: workshops
-            })
-        })
-    })
+            });
+        });
+    });
     
     app.get('/workshop', function (req, res) {
-        console.log("get")
-        res.render('workshop')
-    })
+        console.log("get");
+        res.render('workshop');
+    });
     
     app.post('/workshop', function (req, res) {
-        const name = req.body.name
-        const description = req.body.description
+        const name = req.body.name;
+        const description = req.body.description;
         repository.addWorkshop(name, description).then(() => {
             repository.getWorkshopList()
             .then(workshops => {
                 res.render("index", {
                     workshops: workshops
-                })
-            })
+                });
+            });
         })
-        .catch(e =>res.send(e.message))
-    })
+        .catch(e =>res.send(e.message));
+    });
     
     app.get('/workshop/:name', function (req, res) {
-        const workshopName = req.params.name
+        const workshopName = req.params.name;
         repository.getWorkshopByName(workshopName)
         .then(workshop => {
-            res.render('ejs/workshop', workshop)
+            res.render('ejs/workshop', workshop);
         })
-        .catch(e =>ejs.send(e.message))
-    })
+        .catch(e =>ejs.send(e.message));
+    });
+
     
     app.post('/remove-workshop', function (req, res) {
-        res.status(500).send("TODO")
-    })
+        res.status(500).send("TODO");
+    });
     
-    app.post('/update-workshop', function(req, res) {
-        res.status(500).send("TODO")
-    })
+    app.get('/edit-workshop/:name',
+        function(req, res){
+            const workshopName = req.params.name;
+            repository.getWorkshopByName(workshopName)
+            .then( workshop => {
+                console.log("Workshop:" + workshop.name);
+                
+                res.render('editWorkshop', {workshop: workshop});
+            })
+            .catch(e =>res.send(e.message));
+        }
+    );
+
+    app.post('/update-workshop/', function(req, res) {
+        const workshopName = req.body.name
+        const description = req.body.description
+        repository.updateWorkshop(workshopName, description).then(
+            res.render('index')
+        )
+    });
     
     app.listen(3000, function () {
-      console.log('Workshop app listening on port 3000!')
-    })
+      console.log('Workshop app listening on port 3000!');
+    });
     
-})
+});
 
